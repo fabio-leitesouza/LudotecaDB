5e6f5b2d2af6b958ef93bf034fcab5509e71ab4d
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-09-09 11:08:13 -0300

    Update join.sql

--- join.sql
+++ join.sql
@@ -4,50 +4,52 @@
 
 SELECT Clientes.NomeCliente, Aluguel.AluguelID, Aluguel.DataEntrada
 FROM Clientes
-INNER JOIN Aluguel
-ON Clientes.ClienteID = Aluguel.ClienteID;
+    INNER JOIN Aluguel
+    ON Clientes.ClienteID = Aluguel.ClienteID;
 
 
 SELECT Jogos.NomeJogo, Aluguel.JogoID
 FROM Jogos
-LEFT JOIN Aluguel
-ON Jogos.JogoID = Aluguel.JogoID
+    LEFT JOIN Aluguel
+    ON Jogos.JogoID = Aluguel.JogoID
 WHERE Aluguel.JogoID IS NOT NULL;
 
 
-SELECT * FROM Aluguel;
+SELECT *
+FROM Aluguel;
 
 SELECT JogoID.Nome_Jogo, Aluguel.AluguelID
 FROM Aluguel
-RIGHT JOIN Jogos
-ON JogoID.ID_Jogo = Aluguel.JogoID;
+    RIGHT JOIN Jogos
+    ON JogoID.ID_Jogo = Aluguel.JogoID;
 
 SELECT Jogos.Nome_Jogo, Aluguel.JogoID, Aluguel.AluguelID
 FROM Jogos
-LEFT JOIN Aluguel
-ON Jogos.ID_Jogo = Aluguel.JogoID;
+    LEFT JOIN Aluguel
+    ON Jogos.ID_Jogo = Aluguel.JogoID;
 
 SELECT Jogos.Nome_Jogo, Aluguel.AluguelID
 FROM Jogos
-LEFT JOIN Aluguel
-ON Jogos.ID_Jogo = Aluguel.JogoID;
+    LEFT JOIN Aluguel
+    ON Jogos.ID_Jogo = Aluguel.JogoID;
 
 
-CREATE VIEW ValorAluguelPorClienteData AS
-SELECT
-    c.NomeCliente,
-    a.DataEntrada AS DataAluguel,
-    SUM(j.Preco) AS ValorTotal
-FROM
-    Aluguel a
-JOIN
-    Clientes c ON a.ClienteID = c.ClienteID
-JOIN
-    Jogos j ON a.JogoID = j.JogoID
-GROUP BY
+CREATE VIEW ValorAluguelPorClienteData
+AS
+    SELECT
+        c.NomeCliente,
+        a.DataEntrada AS DataAluguel,
+        SUM(j.Preco) AS ValorTotal
+    FROM
+        Aluguel a
+        JOIN
+        Clientes c ON a.ClienteID = c.ClienteID
+        JOIN
+        Jogos j ON a.JogoID = j.JogoID
+    GROUP BY
     c.NomeCliente,
     a.DataEntrada
-ORDER BY
+    ORDER BY
     c.NomeCliente,
     a.DataEntrada;
 
